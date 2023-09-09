5d8b22f3e7cb87dc3f54f46828a89ab4696d6b2b
Valentina Krasnova <github@krasnova.info>
2023-09-09 14:48:42 +0300

    in progress: copy and modify some files from 'mesto'

---
+++ app.js
@@ -0,0 +1,56 @@
+require('dotenv').config({ path: './key.env' });
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const { rateLimit } = require('express-rate-limit');
+const { errors } = require('celebrate');
+const routes = require('./routes');
+const errorHandler = require('./utils/errors/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const {
+  PORT = 3000,
+  DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+} = process.env;
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'http://movies-ex.ru',
+    'https://movies-ex.ru',
+  ],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(limiter);
+
+mongoose.connect(DB_ADDRESS, {
+  useNewUrlParser: true,
+});
+
+app.use(requestLogger);
+
+app.use(routes);
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT);
